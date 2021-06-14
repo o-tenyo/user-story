57d4263699be242c09e9fc2e71aa3cc3483c3ffa
o-tenyo <johnstone.omundi@student.moringaschool.comgit config --global user.email johnstone.omundi@student.moringaschool.com>
2021-06-14 14:15:19 +0300

    Added new txt file and modified html and css

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+function maleUser(){
+    // if(DD>0 && DD<31 && MM>0 && MM<12){
+    //     d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+    //     console.log(d);
+        if(d==0){
+            console.log('Kwasi');
+        }
+        else if(d==1){
+            console.log('Kwadwo');
+        }
+        else if(d==2){
+            console.log('Kwabena');
+        }
+        else if(d==3){
+            console.log('Kwaku');
+        }
+        else if(d==4){
+            console.log('Yaw');
+        }
+        else if(d==5){
+            console.log('Kofi');
+        }
+        else if(d==6){
+            console.log('Kwame');
+        }}
+    else{
+        console.log('Invalid Data. \nTry Again!');
+    }
+}
+
+function submissionClicked(){
+    if(male===true){
+        maleUser();
+    }
+    else if(female===){
+        femaleUser()
+    }
+}
